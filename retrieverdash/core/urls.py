f06220706eb5c5f7631d17426b580c68aed5c661
Rishika Bhatia <rishikabhatia1703@gmail.com>
2021-04-11 16:11:24 +0530

    Update urls.py

--- retrieverdash/core/urls.py
+++ retrieverdash/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import DashboardView, DiffView
+from .views import DashboardView, DiffView, OldView
 
 app_name = 'core'
 urlpatterns = [
     path('diff/<str:filename>/', DiffView.as_view(), name='diff'),
+    path('old/<str:filename>/', OldView.as_view(), name='old'),
     path('', DashboardView.as_view(), name='dashboard'),
 ]
